6c73d9f68c04226834cccb3362f9856a97349b78
unknown <josue.ham@unitec.edu>
2024-10-28 12:30:24 -0600

    Agregar numeros y validados

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 
 matriz = np.zeros((9, 9), dtype=int)
 
@@ -14,5 +15,27 @@ def imprimir_matriz(matriz):
         
         if (i + 1) % 3 == 0 and i != 8:
             print('-' * 21)
+def es_valido(tablero, fila, col, num):
+    """Verifica si un número puede ser colocado en la posición dada sin violar las reglas del Sudoku."""
+    if num in tablero[fila, :] or num in tablero[:, col]:
+        return False
+    
+    start_row, start_col = 3 * (fila // 3), 3 * (col // 3)
+    if num in tablero[start_row:start_row+3, start_col:start_col+3]:
+        return False
+    
+    return True
 
+def llenar_tablero_aleatorio(tablero):
+    intentos = 0
+    while intentos < 20:  
+        fila = random.randint(0, 8)
+        col = random.randint(0, 8)
+        num = random.randint(1, 9)
+        if tablero[fila, col] == 0 and es_valido(tablero, fila, col, num):
+            tablero[fila, col] = num
+            intentos += 1
+    return tablero
+
+llenar_tablero_aleatorio(matriz)
 imprimir_matriz(matriz)
